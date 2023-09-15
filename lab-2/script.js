4c6c53812218548af9d1912bb8ce5fb3b63a020c
Chao Cao <cao00121@algonquinlive.com>
2023-09-15 17:52:37 -0400

    Completed Lab 2

---
+++ lab-2/script.js
@@ -0,0 +1,12 @@
+//Declare a variable called firstName and assign it.
+const firstName = "Chao"
+
+console.log("My name is " + firstName)
+
+//Declare a variable called favNumber and assign it.
+const favNumber = 7
+
+//Declare a variable called squared and assign it.
+const squared = favNumber * favNumber
+
+console.log(`The squared of ${favNumber} is ${squared}`)
